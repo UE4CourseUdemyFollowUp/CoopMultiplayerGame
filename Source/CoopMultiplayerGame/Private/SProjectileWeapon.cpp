5de288e36e04ba412869f8aa64e2dc355049f8bd
Eduard Korogodov <eduard.korogodov@gmail.com>
2018-02-13 00:00:41 +0200

    launcher and projectile
    Rough made launcher and blueprinted projectile. To be modified later, I
    hope

---
+++ Source/CoopMultiplayerGame/Private/SProjectileWeapon.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CoopMultiplayerGame/Public/SProjectileWeapon.h"
+#include "Components/SkeletalMeshComponent.h"
+
+ASProjectileWeapon::ASProjectileWeapon()
+{
+}
+
+void ASProjectileWeapon::Fire()
+{
+	AActor* Owner = GetOwner();
+
+	if (Owner && ProjectileClass)
+	{
+		FVector EyeLocation;
+		FRotator EyeRotation;
+		Owner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+		FVector MuzzleLocation = MeshComp->GetSocketLocation(MuzzleSocketName);
+
+		FActorSpawnParameters ActorSpawnParams;
+		ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		// EyeRotation to fire the direction we looking, not the gun
+		GetWorld()->SpawnActor<AActor>(ProjectileClass, MuzzleLocation, EyeRotation, ActorSpawnParams);
+	}
+}
+
+
--- Source/CoopMultiplayerGame/Private/SWeapon.cpp
+++ Source/CoopMultiplayerGame/Private/SWeapon.cpp
@@ -11,7 +11,7 @@
 
 // Sets default values
 ASWeapon::ASWeapon()
-	: MuzzleSocketName("MuzzleFlashSocket")
+	: MuzzleSocketName("MuzzleSocket")
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
