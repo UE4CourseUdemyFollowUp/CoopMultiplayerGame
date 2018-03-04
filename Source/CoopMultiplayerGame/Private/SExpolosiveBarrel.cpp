4958e79b699a55acebcf5380492baaf30971a1df
Eduard Korogodov <eduard.korogodov@gmail.com>
2018-03-04 17:53:08 +0200

    Explosive barrel

---
+++ Source/CoopMultiplayerGame/Private/SExpolosiveBarrel.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CoopMultiplayerGame/Public/SExpolosiveBarrel.h"
+#include "CoopMultiplayerGame/Public/Components/SHealthComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Particles/ParticleSystem.h"
+#include "Kismet/GameplayStatics.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+
+// Sets default values
+ASExpolosiveBarrel::ASExpolosiveBarrel()
+	: bExploded(false)
+	, ExplosionImpulse(400.f)
+{
+	HealthComp = CreateDefaultSubobject<USHealthComponent>(TEXT("HealthComp"));
+	HealthComp->OnHealthChanged.AddDynamic(this, &ASExpolosiveBarrel::OnHealthChanged);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetSimulatePhysics(true);
+	MeshComp->SetCollisionObjectType(ECC_PhysicsBody);
+	RootComponent = MeshComp;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComp"));
+	RadialForceComp->SetupAttachment(MeshComp);
+	RadialForceComp->Radius = 250.f;
+	RadialForceComp->bImpulseVelChange = true;
+	RadialForceComp->bAutoActivate = false;
+	RadialForceComp->bIgnoreOwningActor = true;
+
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SetReplicates(true);
+	SetReplicateMovement(true);
+}
+
+void ASExpolosiveBarrel::OnHealthChanged(USHealthComponent * OwningHealthComp, float Health, float HealthDelta, const UDamageType * DamageType, AController * InstigatedBy, AActor * DamageCauser)
+{
+	if (Health <= 0.f && !bExploded)
+	{
+		bExploded = true;
+
+		FVector BoostIntensity = FVector::UpVector * ExplosionImpulse;
+		MeshComp->AddImpulse(BoostIntensity, NAME_None, true);
+
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+
+		MeshComp->SetMaterial(0, ExplodedMaterial);
+
+		RadialForceComp->FireImpulse();
+	}
+}
+
